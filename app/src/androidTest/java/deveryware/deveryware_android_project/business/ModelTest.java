59d92fee9d3c49828f1b77fd59e773a4dfe4d520
Olivier Moreau <olivier.moreau@deveryware.com>
2015-12-02 14:48:38 +0100

    Update tests

---
+++ app/src/androidTest/java/deveryware/deveryware_android_project/business/ModelTest.java
@@ -0,0 +1,30 @@
+package deveryware.deveryware_android_project.business;
+
+import junit.framework.TestCase;
+
+/**
+ * ModelTest
+ * Created by omo-dw on 02/12/15.
+ */
+public class ModelTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testActive() {
+        Model model = new Model();
+        assertTrue(model.isActive());
+    }
+
+    public void testInactive() {
+        Model model = new Model();
+        assertFalse(model.isActive());
+    }
+
+}
---
+++ app/src/main/java/deveryware/deveryware_android_project/business/Model.java
@@ -0,0 +1,15 @@
+package deveryware.deveryware_android_project.business;
+
+/**
+ * Model
+ * Created by omo-dw on 02/12/15.
+ */
+public class Model {
+
+    public Model() {
+    }
+
+    boolean isActive() {
+        return true;
+    }
+}
